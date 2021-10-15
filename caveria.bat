240a11578022fa62b3883b4a244c9f7b04349ca6
Henrique Scander Coelho <60271643+MrSweetRice@users.noreply.github.com>
2021-10-15 09:04:55 -0300

    Add files via upload

---
+++ caveria.bat
@@ -0,0 +1,55 @@
+echo off
+color 0a
+if not "%1" == "max" start /MAX cmd /c %0 max & exit/b                                                                                                                     
+echo         -/                                                                             `.          
+echo         `o+ -`                                                                        .s:          
+echo          .y-:s`                     `-`                `/`                          - oy`          
+echo      --  `ys`so`                     .+/-`          `.:+-                          //.ys   .       
+echo      -o.-+yy/-yo.                      `-:--.....-/+/:.                          `+o.oyy:.-o       
+echo      `yso:.:o/:ss+-.`                      `./syyy+.                        ``.-+so:o+--/sso       
+echo     .sy:`   `:+//ssso+//.                     .yyo`                   `..-:++osso:/+-    `+y/`     
+echo     oy+        ` `.-...-:/-.                   :y.                 `-::--.``````  `       `yy/     
+echo     /yo`   `-.`           .-:-.                .s                 .-`            `.--:-   .ys-     
+echo      -os/  :sso/-`           `.`               `:                             `./oyyy-` `+so.      
+echo       +y/ -o-syyys/.`                                                    `` ./syyyyo+/   oy-       
+echo      `yo `o`oyyyyyyys/.`  `:`                                       :/.-:::+yyyyyyyy-::  `so       
+echo      :y` /-:yyyyyyyyyyys/-`so-                        `           `/yyo++syyyyyyyyyys`+-  -h`      
+echo      /o  ..syyyyyyyyyyyyyyyyyyo-        `o:``..   `  -so:::/:   ./syyyyyyyyyyyyyyyyyy+-o  `y`      
+echo      ``   +yyyyyyyyyyyyyyyyyyyyys+:.```-oyyyhyy` :yssyyyyys:::/oyyyyyyyyyyyyyyyyyyyyyy.o`  -       
+echo          `yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy:`syyo  .yyy/.oyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh:.:`         
+echo          :yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy.  .sy.   +y:` `oyyyyyyyyyyyyyyyyyyyyyyyyyyyyy:`s.         
+echo          oyy+yyyyyyyyyyyyyyyyyyyyyyyyyy:   `y/    `s/   `syyyyyyyyyyyyyyyyyyyyyyyyyyyy`/+          
+echo          ohy`/yyyyyyyyyyyyyyyyyyyyyyys-   `o+      -y.   `+yyyyyyyyyyyyyyyyyyyyyyyysy-./`          
+echo          .s/. .++:...-:://///++oyyso:`    /o`:+.`++.-s.    ./oyyy+/+++/::::::::++++.:              
+echo           ``              ..--::-.`      :s.+yyyyyyy-:o`     ``.--:--.`                            
+echo       -`                                .s-+yyyyyyyyy-//                                   :       
+echo       +-                                +-+yyyyyyyyyys.+-                                 .y`      
+echo       ++                               -:-yyyyyyyyyyyyo`+`                               `/s       
+echo       -y/....                          .`oyyyyyyyyyyyyy::.              ``--:::::      /+oy:       
+echo        -+ossy:::::////::-.` `.``        :yyyyyyyyyyyyyyo`          `..:/oso++//::----::os+-        
+echo          `.-:/oyyysyys/::://:/oo+-`     oyyyyyyyyyyyyyyy.        `oso+//:::/yyyyyyyo/-.``          
+echo                -so./ys:::`  .-`:/yo     syyyyys+/osyyyyy- `..    .y.    `-/+yysoss-                
+echo                 -s`.:yyyoo/     `so    `+sso+//+o+//osss-/so.    `+   ``-oyyy-``s-                 
+echo                  o.y`syyyyy: -.  ``    +//+ooo:+o:/oo++/osy+       `/.syyyyys` .+                  
+echo                  .:s-oyyyyyy`/+::      s:.`s:       :s` `.:s:      -ooyyyyyyo+`:`                  
+echo                    o/oyyyyyyos:/y     .`   `         .     ``    . .yyyyyyyyyh`                    
+echo                    /soyyyyyyy+`ss                                -/ oyyyyyyyys`                    
+echo                    `yyyyyyyyy/`y:                              `  y/osyyyyyyy/                     
+echo                     :syyyyys`syy:/  .    .:           .    .   /:/sy-`syyyyy+                      
+echo                       -oyyys-s.:ys--s`   /:           +/   o--/oh. o--yyyyo-                       
+echo                         /yyoss`:+`-ys//:-s. `   .     :s--:sy:``s. +soyyyy`                        
+echo                          yy:/y:// `s:``.oy+::://s/:-::+y+.`.y   s:-sy-yyys                         
+echo                          /yyy::ss.`s-   +/     :y-    `s-  `y` -ys/-+oyyy-                         
+echo                           +yy/`++-:oo.  o/     `s.     s.  /y-/os/ `+o-s:                          
+echo                            /o-+yo` `y//:os-`   .y-   `:s:-/ys.``+s+ss./-                           
+echo                             .:.ys+++y.  `+o::::sy++///so.``os`.:yy/s:                              
+echo                                /+`-syo.` -+   `+o`````oo  `syo/-s-`/                               
+echo                                `/  :y:--/sy:` `os`  `:ys+/:+s. :/                                  
+echo                                    `y`   :yo//oyyo/::/y+.  +-  :`                                  
+echo                                     /`    o/  `oy-`  -o    y                                       
+echo                                           +/   :y`   ::    -                                       
+echo                                           :/   .s`   :-                                            
+echo                                           `.   .s    .`                                            
+echo                                                `+                                                  
+pause nul                                                                                                  
+                                                                                                    
